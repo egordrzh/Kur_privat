f0792e2fd028d6543160abfefcc4b96848f97394
egordrzh <90134412+egordrzh@users.noreply.github.com>
2023-04-28 15:18:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 def set_request():
@@ -50,11 +52,38 @@ def recognize_request():
     }
 
     response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    return response.text
 
-    print(response.json())
+def get_info_to_str(dict_json):
+    params = ['tag', 'coord', 'sex', 'emotion', 'age']
+    info = dict_json['body']['objects'][0]['persons']
+    result_str = ""
+    with open('Kursach.txt', 'r') as fn:
+        file_name = fn.read()
 
+    # create name dict
+    name_dict = {}
+    for line in file_name.splitlines():
+        name_dict[(line.split(': '))[0]] = (line.split(': '))[1]
+
+    for per in info:
+        if (per[params[0]] != 'undefined'): # if tag != undefined
+            true_name = ""
+            if per[params[0]] in name_dict.keys():
+                true_name = name_dict[per[params[0]]]
+            for param in params:
+                if (param == "tag"):
+                    result_str += str(param) + " - " + str(per[param]) + " " + str(true_name) + '\n'
+                else:
+                    result_str += str(param) + " - " + str(per[param]) + '\n'
+            result_str += "------------------------------\n"
+    return result_str
 
 if __name__ == '__main__':
     set_postman()
-    recognize_request()
-
+    text_json = json.loads(recognize_request())
+    info = get_info_to_str(text_json)
+    print(info)
+    f = open('persons_info.txt', 'w')
+    f.write(info)
+    f.close()
